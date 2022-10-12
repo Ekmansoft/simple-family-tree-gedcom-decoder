55c1e13bc359322c217ded6394012861d0eaee4f
Kenneth Ekman <bkekman@gmail.com>
2022-10-12 16:44:15 +0200

    Fix sync problem in brower

--- src/build-tree-from-gedcom-file.tsx
+++ src/build-tree-from-gedcom-file.tsx
@@ -7,15 +7,19 @@ import * as fs from "fs";
 
 export async function buildTreeFromFile(tree: LocalTreeBackend, filename: string): Promise<boolean>  {
   //console.log("readFileSync:", filename);
-  const gedcomData =  fs.readFileSync(filename, "utf8");
-  //console.log("gedcom length:", gedcomData.length);
-  decodeGedcom(tree, gedcomData);
+  fs.readFile(filename, "utf8", function (err, gedcomData) {
+    if (err == null) {
+      //console.log("gedcom length:", gedcomData.length);
+      decodeGedcom(tree, gedcomData);
 
-  const rootProfile = tree.getRootProfile();
-  if (rootProfile !== undefined) {
-    //console.log("root profile ", rootProfile.itemLink);
-    return true;
-  }
+      const rootProfile = tree.getRootProfile();
+      if (rootProfile !== undefined) {
+        //console.log("root profile ", rootProfile.itemLink);
+        return true;
+      }
+    }
+  })
+
   console.log("failed decoding gedcom");
-  return false;
+  return Promise.resolve(true);
 }
