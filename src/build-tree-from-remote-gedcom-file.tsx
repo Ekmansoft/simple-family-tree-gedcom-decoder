82d46a2fc4b5436e6d1c6a9f2374472d70a08f1b
Kenneth Ekman <bkekman@gmail.com>
2022-10-16 17:19:22 +0200

    Try to improve again

--- src/build-tree-from-remote-gedcom-file.tsx
+++ src/build-tree-from-remote-gedcom-file.tsx
@@ -4,17 +4,17 @@ import axios from "axios";
 
 export async function buildTreeFromRemoteGedcomFile(tree: LocalTreeBackend, filename: string): Promise<boolean>  {
   try {
-    const response = await axios.get(filename);
-    if (!response.status) {
-      throw new Error(`Error! status: ${response.status}`);
+    const { data, status } = await axios.get<string>(filename);
+    if (!status) {
+      throw new Error(`Error! status: ${status}`);
     }
-    const gedcomData = await response.data;
+    const gedcomData = await data;
     decodeGedcom(tree, gedcomData);
     // console.log('success ');
 
     return true;
   } catch (error) {
-    if (error instanceof Error) {
+    if (axios.isAxiosError(error)) {
       console.log('error message: ', error.message);
       return false;
     } else {
